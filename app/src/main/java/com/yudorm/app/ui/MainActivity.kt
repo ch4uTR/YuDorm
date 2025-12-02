39aea5903d4f929a1212156194dbf111749f31db
Enes Yıldırım <enes.yildirim@std.yeditepe.edu.tr>
2025-12-02 12:54:14 +0300

    Add: LoginScreen.kt created

--- app/src/main/java/com/yudorm/app/ui/MainActivity.kt
+++ app/src/main/java/com/yudorm/app/ui/MainActivity.kt
@@ -4,14 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.yudorm.app.ui.screens.LoginScreen
 import com.yudorm.app.ui.theme.YUDormTheme
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.yudorm.app.ui.screens.HomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +17,27 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             YUDormTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
+                val navController = rememberNavController()
+
+                NavHost(navController = navController, startDestination = "login"){
+
+
+                    composable("login"){
+                        LoginScreen(
+                            onLoginSuccess =  { studentNo -> navController.navigate("home/$studentNo") },
+                            onPasswordResetButton = { navController.navigate("login")},
+                            onAuthorityLoginButton = {navController.navigate("login")                 },
+                            )
+                    }
+
+                    composable("home/{studentNo}") { backStackEntry ->
+                        val studentNoRetrieved = backStackEntry.arguments?.getString("studentNo")
+                        HomeScreen(studentNo =  studentNoRetrieved ?: "")
+                    }
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    YUDormTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/yudorm/app/ui/screens/LoginScreen.kt
@@ -0,0 +1,151 @@
+package com.yudorm.app.ui.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.yudorm.app.ui.viewmodel.LoginViewModel
+import com.yudorm.app.R
+
+val AppBlue = Color(0xFF2F76BD)
+val AppOrange = Color(0xFFF59E48)
+
+
+@Composable
+fun LoginScreen(
+    viewModel: LoginViewModel = viewModel(),
+    onLoginSuccess: (String?) -> Unit,
+    onPasswordResetButton: () -> Unit,
+    onAuthorityLoginButton: () -> Unit) {
+
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.yudorm_logo),
+            contentDescription = "Logo",
+            modifier = Modifier
+                .size(250.dp)
+        )
+
+
+        Spacer(modifier = Modifier.height(48.dp)) // Boşluk
+
+
+        Text(
+            text = "Öğrenci no",
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 8.dp),
+            color = Color.Black
+        )
+
+        OutlinedTextField(
+            value = viewModel.studentNo,
+            onValueChange = { viewModel.studentNo = it },
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(24.dp),
+            singleLine = true,
+            colors = OutlinedTextFieldDefaults.colors(
+                unfocusedContainerColor = Color.White,
+                focusedContainerColor = Color.White
+            )
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+
+        Text(
+            text = "Şifre",
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 8.dp),
+            color = Color.Black
+        )
+
+        OutlinedTextField(
+            value = viewModel.password,
+            onValueChange = { viewModel.password = it },
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(24.dp),
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            colors = OutlinedTextFieldDefaults.colors(
+                unfocusedContainerColor = Color.White,
+                focusedContainerColor = Color.White
+            )
+        )
+
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+
+        Text(
+            text = "Şifrenizi mi unuttunuz?",
+            color = AppBlue,
+            fontWeight = FontWeight.SemiBold,
+            modifier = Modifier
+                .align(Alignment.End)
+                .clickable { onPasswordResetButton }
+                .padding(vertical = 8.dp)
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+
+        Button(
+            onClick = {
+                viewModel.login()
+                if (viewModel.message == "Giriş Başarılı!"){
+                    onLoginSuccess(viewModel.studentNo)
+                }
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = AppBlue),
+            shape = RoundedCornerShape(24.dp)
+        ) {
+            Text(text = "Giriş Yap", fontSize = 18.sp)
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+
+        Button(
+            onClick = { onAuthorityLoginButton},
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = AppOrange),
+            shape = RoundedCornerShape(24.dp)
+        ) {
+            Text(text = "Yetkili Girişi", fontSize = 18.sp)
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(text = viewModel.message)
+    }
+}
